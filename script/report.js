1c2997624b633ca90865571255955a5d2383c0bd
NVH <93785793+ngvihoa@users.noreply.github.com>
2023-04-20 00:16:11 +0700

    update report 1.1

--- script/report.js
+++ script/report.js
@@ -1,3 +1,15 @@
 
-let a = Date();
-console.log(a);
+// let a = Date();
+// console.log(a);
+
+let listLi = document.querySelectorAll('.report-topic > ul li');
+
+listLi.forEach((e)=>{
+    e.addEventListener("click", ()=>{
+        listLi.forEach((i)=>{
+            i.classList.remove('aim');
+        }, 100);
+        e.classList.add('aim');
+    })
+}, 100);
+
