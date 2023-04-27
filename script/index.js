456362bdd8617ef2d26ad5533156d32fef6044fa
NVH <93785793+ngvihoa@users.noreply.github.com>
2023-04-27 15:18:36 +0700

    update index

--- script/index.js
+++ script/index.js
@@ -2,7 +2,8 @@ const url = new URL("https://accounts.google.com/o/oauth2/v2/auth");
 const params = new URLSearchParams({
     client_id: "386980888510-c9dpabmmacg9la2rp6cnkvgg49kooapp.apps.googleusercontent.com",
     prompt: "select_account",
-    redirect_uri: "http://localhost:8080/login",
+    redirect_uri: "../page/dashboard.html",
+                    // http://localhost:8080/login
     response_type: "code",
     scope: "profile email"
 });
