6649482db523734840a006373f209b21430e4592
CT08\CT08 <pcthanh0802@gmail.com>
2023-05-06 16:21:10 +0700

    add username to sidebar

--- public/script/sidebar.js
+++ public/script/sidebar.js
@@ -5,3 +5,8 @@ let sidebar = document.querySelector('.sidebar');
 btn.onclick = ()=>{
     sidebar.classList.toggle('active');
 }
+
+if (sessionStorage.username === undefined)
+    sessionStorage.username = JSON.parse(atob(document.cookie.split('=')[1].split('.')[1])).userEmail.split('@')[0]
+
+document.getElementById('user_email').innerText = sessionStorage.username
