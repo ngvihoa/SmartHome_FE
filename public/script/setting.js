49ee8c44bafa04a9610df51dccdeb8b4710db7c5
NVH <93785793+ngvihoa@users.noreply.github.com>
2023-05-09 12:16:11 +0700

    update automod constraint

--- public/script/setting.js
+++ public/script/setting.js
@@ -5,3 +5,23 @@ const user_email = document.getElementById('user_email');
 user_email.innerText = sessionStorage.getItem('user_name');
 
 const settingPage = new Setting(document.getElementById('main-content'))
+
+const automodbutton = document.querySelectorAll('input[type="checkbox"]')
+
+automodbutton.forEach(e=>{
+    e.addEventListener('click',a=>{
+        let tmp = a.srcElement.checked;
+        let intp = a.srcElement.parentNode.querySelectorAll('input[type="time"]');
+        if(tmp){
+            intp.forEach(s=>{
+                s.disabled = false;
+            })
+        }
+        else{
+            intp.forEach(s=>{
+                s.disabled = true;
+            })
+        }
+
+    })
+})
